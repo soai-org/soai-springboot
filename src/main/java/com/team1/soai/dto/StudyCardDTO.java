c0eadac2522471f1d300c2b82ad212dbd80d02e9
Yudir-98 <wjsdbqja156@gmail.com>
2025-08-28 10:03:47 +0900

    Studies PatientUuid로 검색 Pagination 서비스 코드

--- src/main/java/com/team1/soai/dto/StudyCardDTO.java
+++ src/main/java/com/team1/soai/dto/StudyCardDTO.java
@@ -1,16 +1,27 @@
 package com.team1.soai.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudyCardDTO {
     private String studyUuid;
     private String studyDate;
+    private String studyTime;
     private String studyDescription;
-    private String modality;
-    private int seriesCount;
-    private int instanceCount;
+
     private String thumbnailInstanceUuid; // 썸네일용 Instance UUID
     private String patientId;
     private String patientName;
+    private String patientSex;
+    
+    public StudyCardDTO(String studyUuid, String studyDate, String patientName, String studyDescription) {
+        this.studyUuid = studyUuid;
+        this.studyDate = studyDate;
+        this.patientName = patientName;
+        this.studyDescription = studyDescription;
+    }
 }
