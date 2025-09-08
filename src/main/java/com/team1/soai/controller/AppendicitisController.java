69d789884835a2683de4f70609acaeb9e6ba8c7f
Hwang Youseong <hysskm@gmail.com>
2025-09-08 12:04:38 +0900

    충수염 반환값 오류 수정 API

--- src/main/java/com/team1/soai/controller/AppendicitisController.java
+++ src/main/java/com/team1/soai/controller/AppendicitisController.java
@@ -3,6 +3,7 @@ package com.team1.soai.controller;
 import com.team1.soai.dto.AppendicitisRequest;
 import com.team1.soai.dto.AppendicitisResponse;
 import com.team1.soai.service.AppendicitisDiagnosisService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
